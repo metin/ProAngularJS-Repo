21f081a8d4912819dfffac36bab2342763d398a7
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2015-12-30 19:04:58 +0100

    Add ToDo app with backend integration

---
+++ part01-getting-ready/01-hello-angular/004-todo-app-backend-integration/public/js/app.js
@@ -0,0 +1,72 @@
+(function(){
+  "use strict";
+
+  var model = {
+    user: "Sergio"
+  };
+
+  /* the AngularJS application */
+  var app = angular.module("todoApp", ["todoAppHeaderModule", "todoAppTaskFormModule"]);
+
+  /* initialization tasks */
+  app.run(["$http", function($http) {
+    model.items = [];
+    $http
+      .get("http://spark:8080/tasks/sergio")
+      .success(function(data) {
+        model.items = data;
+      });
+  }]);
+
+  /*
+    The Controller:
+      + user action responses
+      + glue between the model and the view
+  */
+  app.controller("ToDoCtrl", function() {
+
+    /* bind the model in the `todoInfo` property */
+    this.toDoInfo = model;
+
+    /* return the list of tasks not completed */
+    this.getPendingTasks = function() {
+      return model.items.filter(function(item) {
+        return !item.done;
+      });
+    };
+
+    /* return bootstrap class `label-success` if pending tasks < 3; `label-warning` otherwise */
+    this.getWarningLevelClass = function() {
+      var pendingTasksCount = this.getPendingTasks().length;
+      if (pendingTasksCount < 3) {
+        return "label-success";
+      } else {
+        return "label-warning";
+      }
+    };
+
+    /* placeholder for the new Task */
+    this.task = {};
+
+    /* add a new task to the model */
+    this.addNewItem = function(actionText) {
+      model.items.push({ action: actionText, done: false});
+      this.task = {};
+    };
+
+    /* default filter value */
+    this.showCompleted = true;
+  });
+
+  /*
+    filterByCheckedStatus Custom filter
+  */
+  app.filter("filterByCheckedStatus", function() {
+    return function(items, showCompleted) {
+      return items.filter(function(item) {
+        return !item.done || showCompleted;
+      });
+    };
+  });
+
+})();
